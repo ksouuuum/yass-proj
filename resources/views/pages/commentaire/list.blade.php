0cadfc788d8404df7ebbeaa79088b23b2b57c1f8
ksouuuum <yaluge123@gmail.Com>
2024-07-20 17:05:01 +0200

    adding some more stuff

--- resources/views/pages/commentaire/list.blade.php
+++ resources/views/pages/commentaire/list.blade.php
@@ -4,7 +4,7 @@
 <div id="" class="mt-8 ">
 
     <div class="flex flex-col gap-2 justify-center ">
-        <h2 class="text-violet-500 text-xl md:text-2xl font-bold px-2"> Liste de tout les commentaire du site   </h2>
+        <h2 class="text-violet-500 text-xl md:text-2xl font-bold px-2"> Liste de tout les commentaires du site   </h2>
 
      
         
@@ -18,8 +18,8 @@
                 <div class="w-80 mt-1 flex flex-col gap-1 ">
                             
                             <div class="flex flex-col gap-1  items-center mt-1 space-y-0 ">
-                                <h1 class="font-bold text-violet-500 text-md leading-tight">{{ $row->id}}</h1>
-                                <h2 class="font-bold text-violet-500 text-md leading-tight">{{ $row->user->name}}</h2>
+                                <h1 class="font-bold text-violet-500 text-md leading-tight">{{ $row->id }} - {{ $row->user->name}}</h1>
+                                <h2 href="{{route('ressource', ['id'=>$row->ressource->id])}}" class="font-bold text-violet-500 text-md leading-tight">{{ $row->ressource->titre}}</h2>
                                 <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   {{ Str::limit($row->corps, 50) }} </p>
                                 <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   {{ $row->created_at}} </p>
                                 <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   
