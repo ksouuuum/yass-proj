04303c3fbf15b4958df278858c8e81aaa1cabdce
ksouuuum <yaluge123@gmail.Com>
2024-07-20 18:52:23 +0200

    typing error fix

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -50,7 +50,7 @@
                         </a>
                     </li>   
                     <li class="pl-2 mt-2 mb-2">    
-                        <a class="text-blue-500 hover:text-blue-800 flex flex-row items-center gap-1 " href="{{route('gestcomm')}}">Modération
+                        <a class="text-blue-500 hover:text-blue-800 flex flex-row items-center gap-1 " href="{{route('gestcom')}}">Modération
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -109,7 +109,7 @@
                         </a>
                     </li>
                     <li>    
-                        <a class="text-blue-500 hover:text-blue-800 flex flex-row " href="{{route('gestcomm')}}">Modération
+                        <a class="text-blue-500 hover:text-blue-800 flex flex-row " href="{{route('gestcom')}}">Modération
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
