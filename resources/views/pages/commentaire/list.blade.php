e6427da88fdc67f372c750f6afacd4f036e1d53d
ksouuuum <yaluge123@gmail.Com>
2024-07-20 15:39:21 +0200

    last fix on god

--- resources/views/pages/commentaire/list.blade.php
+++ resources/views/pages/commentaire/list.blade.php
@@ -12,11 +12,7 @@
         <div class="w-full min-h-screen flex flex-row gap-4 flex-wrap justify-center  items-start text-sm text-violet-500  "> 
             @forelse($commentaires as $index => $row)
                 <!-- Le cadre d'une ressource  -->
-                <div class="w-80 mt-1 flex flex-col gap-1 ">
-                            <div class=" flex flex-col  w-2/3 mx-auto aspect-square ">
-                                <img class="h-40 object-cover rounded-xl" src="{{ route('res-img', [ 'fn' => $row->media ]);}}">
-                            </div>
-                            
+                <div class="w-80 mt-1 flex flex-col gap-1 ">       
                             <div class="flex flex-col gap-1  items-center mt-1 space-y-0 ">
                                 <h1 class="font-bold text-violet-500 text-md leading-tight">{{ $row->id}}</h1>
                                 <p class=" mt-4 mb-4 text-slate-900 border-b border-t border-slate-300 ">   {{ Str::limit($row->corps , 20) }}</p>
